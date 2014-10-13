931f0334fe644d3735851f88af0347525bad9e67
Logan Sherwood <powerdoom@163.com>
2014-10-14 00:28:27 +0800

    Package Require

---
+++ webApp/js/script.js
@@ -0,0 +1,18 @@
+/************************************************************************
+ *  Version: Beta - 0.0.0                                               *
+ *  Author: Lorgan Sherwood                                             *
+ *  DataTime: 2014/10/13                                                *
+ *  Description:                                                        *
+ *  Copyright ©2014 ONO-191 Studio. All Rights Reserved.                *
+ ***********************************************************************/
+
+var OnoStudioApp = angular.module('OnoStudioApp', []);
+
+OnoStudioApp.factory('StudioFactory', function() {
+
+});
+
+OnoStudioApp.controller('MainController', function() {
+
+});
+
