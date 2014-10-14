09031175a246dd795a6c6a49d8cae03edb1e9868
Logan Sherwood <powerdoom@163.com>
2014-10-14 17:12:45 +0800

    test

--- webApp/js/script.js
+++ webApp/js/script.js
@@ -8,12 +8,56 @@
 
 var OnoStudioApp = angular.module('OnoStudioApp', []);
 
-OnoStudioApp.factory('StudioFactory', function() {
+OnoStudioApp.factory('StudioFactory', function($http, $q) {
   var service = {};
+  var baseUrl = 'https://itunes.apple.com/search?term=';
+  var _artist = '';
+  var _finaUrl = '';
+
+  var makeUrl = function() {
+    _artist = _artist.split(' ').join('+');
+    _finaUrl = baseUrl + _artist + '%callback=JSON_CALLBACK'
+    return _finaUrl;
+  }
+
+  service.setArtist = function(artist) {
+    _artist = artist;
+  }
+
+  service.getArtist = function() {
+    return _artist;
+  }
+
+  service.callItunes = function() {
+    makeUrl();
+    var deferred = $q.defer();
+    $http({
+      method: 'JSONP',
+      url: _finaUrl
+    }).success(function(data) {
+        deferred.resolve(data);
+    }).error(function() {
+        deferred.reject('There was an error')
+    })
+    return deferred.promise;
+  }
+
   return service;
 });
 
-OnoStudioApp.controller('MainController', function() {
+OnoStudioApp.controller('StudioFactoryController', function($scope, StudioFactory) {
+  $scope.data = {};
+  $scope.updateArtist = function() {
+    StudioFactory.setArtist($scope.data.artist);
+  };
 
+  $scope.submitArtist = function() {
+    StudioFactory.callItunes()
+      .then(function(data) {
+          $scope.data.artistData = data;
+        }, function(data) {
+          alert(data);
+        })
+  }
 });
 
